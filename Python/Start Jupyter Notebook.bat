631241dff745d7346d14f4f3252ae69468177cce
laviethan49 <laviethan49@gmail.com>
2025-05-27 10:55:50 -0400

    Moving things around to bemore organized

--- Python/Start Jupyter Notebook.bat
+++
@@ -1,2 +0,0 @@
-cd "C:\Users\Public\Documents\Git\JAX-Python-Img-Processing"
-%windir%\system32\cmd.exe "/K" C:\Users\savile\AppData\Local\miniforge3\Scripts\activate_jupyter.bat GPU-Image-Processing
